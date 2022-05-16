24d40c45d38326e0bd3350705c2387c02c9ef0de
muellerpeter8 <105322445+muellerpeter8@users.noreply.github.com>
2022-05-16 01:11:22 -0600

    src files

---
+++ src/features/tours/useCart.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export const useCart = () => {
+    const [cartTotal, setCartTotal] = useState(0);
+    const [cart, setCart] = useState([]);
+
+    const addToCart = (tourItem) => {
+        setCart(cart => [...cart, tourItem]);
+        setCartTotal(cartTotal + tourItem.price);
+    }
+
+    const deleteFromCart = (tourItem) => {
+        setCart(cart.filter(cartItem => cartItem.id !== tourItem.id));
+        setCartTotal(cartTotal - tourItem.price);
+    }
+    const cartCount = cart.length;
+
+    return {
+        cartTotal,
+        cartCount,
+        addToCart,
+        deleteFromCart
+    }
+}
